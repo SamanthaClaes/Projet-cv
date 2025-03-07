7b7574502360b0450a522396a0d86dbc675d2382
SamanthaClaes <Samantha.claes@student.hepl.be>
2025-03-07 17:29:28 +0100

    js work !!!

--- js/main.js
+++ js/main.js
@@ -1,44 +1,52 @@
-const buttonShowMore = document.getElementById('show_text');
+/*Modal*/
 const overlayElement = document.getElementById('overlay');
 const modalElement = document.getElementById('modal');
 const closeModalBtn = document.getElementById('close-modal-btn');
-const hideText = document.getElementById('hidden');
+
+/*buttons*/
+
+const showMoreButton = document.getElementById('show_text');
 const formationButton = document.getElementById('button_formation');
-const tinytext = document.getElementById('tiny_text_test');
-const expText = document.getElementById('exp_text');
+const experienceButton = document.getElementById('button_experience');
+const descriptionButton = document.getElementById('button_description');
+
+/*Contenu*/
+
 const formationText = document.getElementById('formation_text');
-const expButton = document.getElementById('button_experience')
-const descButton = document.getElementById('button_description');
+const experienceText = document.getElementById('exp_text');
+const descriptionText = document.getElementById('tiny_text_test');
 
 
-formationButton.addEventListener('click', () => {
-    tinytext.classList.add('hidden')
-    expText.classList.add('hidden')
-    formationText.classList.remove('hidden')
-})
-expButton.addEventListener('click', () => {
-    tinytext.hidden = true;
-    expText.hidden = false;
-    formationText.hidden = true;
-})
-descButton.addEventListener('click', () => {
-    tinytext.hidden = false;
-    expText.hidden = true;
-    formationText.hidden = true;
+descriptionButton.addEventListener('click',()=>{
+    formationText.hidden=true;
+    experienceText.hidden=true;
+    descriptionText.hidden=false;
 })
 
 
+formationButton.addEventListener('click',()=>{
+    formationText.hidden=false;
+    experienceText.hidden=true;
+    descriptionText.hidden=true;
+})
 
-buttonShowMore.addEventListener('click', () => {
+experienceButton.addEventListener('click',()=>{
+    formationText.hidden=true;
+    experienceText.hidden=false;
+    descriptionText.hidden=true;
+})
+
+
+
+
+showMoreButton.addEventListener('click', () => {
     overlayElement.style.display = 'block'; // Affiche l'overlay
     modalElement.style.display = 'block'; // Affiche la modale
     setTimeout(() => {
         modalElement.style.right = '0'; // Fait glisser la modale depuis la droite
     }, 10);
-    hideText.classList.remove('hidden_text')
-});
+})
 
-// Fermer la modale
 closeModalBtn.addEventListener('click', () => {
     modalElement.style.right = '-100%'; // Fait glisser la modale vers la droite
     overlayElement.style.display = 'none'; // Masque l'overlay
